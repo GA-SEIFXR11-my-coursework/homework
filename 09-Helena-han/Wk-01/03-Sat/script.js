991325c9d6a0e0d8cd805095e1ea76409f959add
chelsie-fu <chelsie.fu@rea-group.com>
2023-02-28 20:09:27 +1100

    w1d1+w1d3-hw

---
+++ 09-Helena-han/Wk-01/03-Sat/script.js
@@ -0,0 +1,13 @@
+ 
+let askQuestion1=prompt ("what buger would you like?")
+console.log(askQuestion1);
+
+let askQuestion2=prompt ("Would you like fries with that?")
+
+if (askQuestion2 === "yes") {
+    console.log(`So that's 1 ${askQuestion1} with fries.`);
+  } 
+    else {
+    console.log(`So that's 1 ${askQuestion1}, no fries.`);
+  }
+
