bcb41418cfc582d21813e5dcc946eef32a8d9943
axwong <alexwong@tadyen.dev>
2023-04-15 16:16:00 +1000

    cleanup everyone elses stuff

--- 08-Grace-Otto/Wk-01/03-Sat/burgerOrders/README.md
+++
@@ -1,3 +0,0 @@
-This is my W1D3 homework, burger orders.
-
-Frog GIF sourced from: https://giphy.com/stickers/food-trucks-nagle-one-fat-frog-EF92elKzLDMlrI60Xh
--- 13-Kyle-Kovac/Wk-01/03-Sat/classwork/login.js
+++
@@ -1,8 +0,0 @@
-function login(form){
-    if(form.user.value === "a" && form.password.value === "a"){
-        location.href = "homework.html"
-    }else{
-    var passuser = document.getElementById("incorrect")
-    passuser.innerHTML = "password incorrect asdasd"
-}
-}
