00ca36b58b11ffc2e3539840077a22722c08ce99
axwong <alexwong@tadyen.dev>
2023-04-22 01:57:30 +1000

    Mild refactor. It now gives a list of movies but does not give specific movie. More work tbd

--- 01-Alex-Wong/Wk-09/01-Tues-MovieAPI/flaskapp.py
+++ 01-Alex-Wong/Wk-09/01-Tues-MovieAPI/flaskapp.py
@@ -1,44 +1,89 @@
-from flask import Flask, render_template, request as req
+from flask import Flask, render_template, redirect, request as req
 from enum import Enum
 
 import requests
 
-class Movie_Search_Options(Enum):
-    BY_SEARCH = "by_search"
-    BY_TITLE = "by_title"
 
 app = Flask(__name__)
+class Movie_Searcher():
+    class Options(Enum):
+        BY_SEARCH = "by_search"
+        BY_TITLE = "by_title"
 
-def read_apikey() -> str:
-    filename = ".apikey"
-    fp = open(filename, "r")
-    apikey = fp.readline()
-    fp.close()
-    return apikey
-
-def search_movies(option: Movie_Search_Options, apikey:str, title:str):
     url_base = "https://www.omdbapi.com/"
-    url_api = f"?apikey={apikey}"
-    title_formatted = "+".join(title.split())
-    match option:
-        case Movie_Search_Options.BY_SEARCH:
-            url_search = f"s={title_formatted}"
-        case Movie_Search_Options.BY_TITLE:
-            url_search = f"t={title_formatted}"
-        case _:
-            raise ValueError(f"Invalid option: {option}")
-    url = f"{url_base}{url_api}&{url_search}"
-    response = requests.get(url).json()
-    return response
+    apikey = ""
+    title = ""
+    response: any
+    
+    def read_apikey(self) -> str:
+        filename = ".apikey"
+        fp = open(filename, "r")
+        apikey = fp.readline()
+        fp.close()
+        self.apikey = apikey
+        return apikey
+
+    def _search_movies(self, option: Options):
+        url_base = self.url_base
+        url_api = f"?apikey={self.apikey}"
+        title_formatted = "+".join(self.title.split())
+        match option:
+            case self.Options.BY_SEARCH:
+                url_search = f"s={title_formatted}"
+            case self.Options.BY_TITLE:
+                url_search = f"t={title_formatted}"
+            case _:
+                raise ValueError(f"Invalid option: {option}")
+        url = f"{url_base}{url_api}&{url_search}"
+        response = requests.get(url).json()
+        self.response = response
+        return response
+    
+    def search_all_movies(self):
+        return self._search_movies(self.Options.BY_SEARCH)
+    
+    #  Need to implement a "by ID" search to get specifc movie
+    def search_specific_movie(self):
+        return self._search_movies(self.Options.BY_TITLE)
+    
+    
+    
+    def set_title(self, title):
+        self.title = title
+        return
+        
+movie_searcher = Movie_Searcher()
+movie_searcher.read_apikey()
 
 @app.route("/")
-def html_root():
-    return render_template("base.html")
+def html_home():
+    return render_template("home.html")
     
+@app.route("/movie_not_found")
+def html_not_found():
+    return render_template("movie_not_found.html")
+
 @app.route("/movie")
-def html_root():
+def html_movie():
+    
     return render_template("movie.html")
 
+@app.route("/movies")
+def html_movies():
+    movies = movie_searcher.response["Search"]
+    return render_template("movies.html", movies = movies)
+
+@app.route("/get_title")
+def get_title():
+    title = req.args.get("title")
+    movie_searcher.set_title(title)
+    response = movie_searcher.search_all_movies()
+    if response["Response"] == "False":
+        return redirect("/movie_not_found")
+    if response["totalResults"] == "1":
+        return redirect("/movie")
+    return redirect("/movies")
+ 
 if __name__ == "__main__":
     app.run(debug=True)
     
