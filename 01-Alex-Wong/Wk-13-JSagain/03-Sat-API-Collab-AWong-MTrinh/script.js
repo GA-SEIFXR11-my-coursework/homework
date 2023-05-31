afced0378361da8e41eaf95ddbe2aacddaab91dd
axwong <alexwong@tadyen.dev>
2023-05-31 18:01:49 +1000

    Added functionality to retrieve pokemon info on search

--- 01-Alex-Wong/Wk-13-JSagain/03-Sat-API-Collab-AWong-MTrinh/script.js
+++ 01-Alex-Wong/Wk-13-JSagain/03-Sat-API-Collab-AWong-MTrinh/script.js
@@ -5,18 +5,33 @@ var listOfPokemon = [
 axios.get('https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0')
   .then(function(response){
     let pokelist = response.data.results;
-    for(let pokemon of pokelist){
+    for(let index in pokelist){
+      let pokemon = pokelist[index]
       listOfPokemon.push({
         title: pokemon["name"],
-        url: pokemon["url"]
+        id: (Number(index)+1),
+        apiurl: pokemon["url"]
       })
     }
   })
   .then(function(){
     $('.ui.search')
       .search({
-        source: listOfPokemon
+        source: listOfPokemon,
+        onSelect: function(result,response){
+          // result is the selected result
+          // response is the filtered list of results
+          getPokemonInfo(result)
+          return true
+        }
       })
     ;
   })
 
+function getPokemonInfo(result){
+  let url = result["apiurl"]
+  axios.get(url)
+    .then(function(response){
+      console.log(response.data)
+    })
+}
