0787ef7e4d6c6ac303c10da684b7f399cb20830f
michaeltrinh <asdf.michael@hotmail.com>
2023-05-31 21:46:03 +1000

    display abilities in a list

--- 01-Alex-Wong/Wk-13-JSagain/03-Sat-API-Collab-AWong-MTrinh/script.js
+++ 01-Alex-Wong/Wk-13-JSagain/03-Sat-API-Collab-AWong-MTrinh/script.js
@@ -63,34 +63,39 @@ function updatePokemonTypeDropdown(){
   ;
 }
 
-function getPokemonInfo(result){
-  let url = result["apiurl"]
-  axios.get(url)
-    .then(function(response){
-        let data = response.data
-        let types = data['types']
-        let pokemonName = data['name']
-        let artWork = data['sprites']['other']['official-artwork']['front_default']
-        let abilities = data['abilities']
-        for(let item of types) {
-            let type = item['type']['name']
-            console.log(type);
-            document.getElementById('type').textContent = `Type: ${type}`
-        }   for(let ability of abilities) {
-            let abilityName = ability['ability']['name']
-            console.log(abilityName);
-            let abilityElement = document.createElement('p')
-            abilityElement.textContent = `Abilities: ${abilityName}`
-            document.getElementById('abilities').appendChild(abilityElement)
-        }   
-        console.log(pokemonName)
-        console.log(artWork)
-        console.log(response.data)
+function getPokemonInfo(result) {
+    let url = result["apiurl"];
+    axios.get(url)
+      .then(function(response) {
+        let data = response.data;
+        let types = data['types'];
+        let pokemonName = data['name'];
+        let artWork = data['sprites']['other']['official-artwork']['front_default'];
+        let abilities = data['abilities'];
+        
+        for (let item of types) {
+          let type = item['type']['name'];
+          document.getElementById('type').textContent = `Type: ${type}`;
+        }
+        let abilitiesContainer = document.createElement('div');
+        let abilitiesTitle = document.createElement('p');
+        abilitiesTitle.textContent = 'Abilities:';
+        abilitiesContainer.appendChild(abilitiesTitle);
 
-        document.getElementById('pokemonName').textContent = `Name: ${pokemonName}`
-        document.getElementById('artWork').src = artWork
-
-    })
-}
+        let abilitiesList = document.createElement('ul');
+        abilitiesList.style.listStyleType = 'none';
+        
+        for (let ability of abilities) {
+          let abilityName = ability['ability']['name'];
+          let abilityElement = document.createElement('li');
+          abilityElement.textContent = `• ${abilityName}`;
+          abilitiesList.appendChild(abilityElement);
+        }
+        abilitiesContainer.appendChild(abilitiesList);
+        document.getElementById('abilities').appendChild(abilitiesContainer);
+        document.getElementById('pokemonName').textContent = `Name: ${pokemonName}`;
+        document.getElementById('artWork').src = artWork;
+      });
+  }
 
 
